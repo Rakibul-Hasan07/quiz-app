6c36a9a50cd8dbe89dd86ca53074f49fddc34c22
shihab00027 <shihabhasan981@gmail.com>
2022-10-11 17:40:01 +0600

    ReChart Added

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -10,10 +10,15 @@ const Navbar = () => {
             </div>
             <div className="flex-none">
                 <ul className="menu menu-horizontal p-0 font-bold">
-                    <NavLink to='/home'>Home</NavLink>
-                    <NavLink to='/topics'>Topics</NavLink>
-                    <NavLink to='/statics'>Statics</NavLink>
-                    <NavLink to='/blog'>Blog</NavLink>
+                    <NavLink to='/topics'className={({ isActive }) => {
+                        return isActive ? "bg-pink-400 rounded-md px-2 py-1" : undefined;
+                    }}>Topics</NavLink>
+                    <NavLink to='/statics'className={({ isActive }) => {
+                        return isActive ? "bg-pink-400 rounded-md px-2 py-1" : undefined;
+                    }}>Statics</NavLink>
+                    <NavLink to='/blog'className={({ isActive }) => {
+                        return isActive ? "bg-pink-400 rounded-md px-2 py-1" : undefined;
+                    }}>Blog</NavLink>
                 </ul>
             </div>
         </div>
--- src/components/Routes/routes.js
+++ src/components/Routes/routes.js
@@ -1,4 +1,5 @@
 import Blog from "../Blog/Blog";
+import Cart from "../Cart/Cart";
 import QuizTopic from "../QuizTopic/QuizTopic";
 import Root from "../Root/Root";
 import Statics from "../Statics/Statics";
@@ -10,16 +11,22 @@ const { createBrowserRouter } = require("react-router-dom");
 export const router = createBrowserRouter([
     {
         path: '/', element: <Root></Root>, children: [
-            { path: '/', element: <TopicCart></TopicCart>, loader: () => {
-                return fetch('https://openapi.programming-hero.com/api/quiz')
-            } },
-            { path: '/home', element: <TopicCart></TopicCart>, loader: () => {
-                return fetch('https://openapi.programming-hero.com/api/quiz')
-            } },
-            {path:'/topic/:topicId', element:<QuizTopic></QuizTopic>, loader: ({params})=>{
-                return fetch(`https://openapi.programming-hero.com/api/quiz/${params.topicId}`)
-            }},
-            { path: '/topics', element: <Topics></Topics> },
+            {
+                path: '/', element: <TopicCart></TopicCart>, loader: () => {
+                    return fetch('https://openapi.programming-hero.com/api/quiz')
+                }
+            },
+            {
+                path: '/topics', element: <TopicCart></TopicCart>, loader: () => {
+                    return fetch('https://openapi.programming-hero.com/api/quiz')
+                }
+            },
+            {
+                path: '/topic/:topicId', element: <QuizTopic></QuizTopic>, loader: ({ params }) => {
+                    return fetch(`https://openapi.programming-hero.com/api/quiz/${params.topicId}`)
+                }
+            },
+           
             { path: '/statics', element: <Statics></Statics> },
             { path: '/blog', element: <Blog></Blog> }
         ]
