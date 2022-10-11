1877434867076a5a37b411456a9e34d36da02e45
shihab00027 <shihabhasan981@gmail.com>
2022-10-11 21:43:16 +0600

    Navbar Responsive done

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,30 +1,32 @@
-import React from 'react';
 import { NavLink } from 'react-router-dom';
-import './Navbar.css'
+import './Navbar.css';
 
 const Navbar = () => {
     return (
-        <div className="sm:flex flex-col navbar bg-base-100 bg-pink-200 shadow-lg px-16">
-            <div className="flex-1">
-                <p className="normal-case text-xl font-bold sm:mb-7">RH-Quiz-<span className='text-pink-600'>Contest</span></p>
+        <nav>
+            <div className="flex flex-col sm:flex-row navbar bg-base-100 bg-pink-200 shadow-lg px-16">
+                <div className="flex-1">
+                    <p className="normal-case text-xl font-bold sm:mb-7">RH-Quiz-<span className='text-pink-600'>Contest</span></p>
+                </div>
+                <div>
+                    <ul className="menu menu-horizontal p-0 font-bold flex flex-col sm:flex-row">
+                        <NavLink to='/home' className={({ isActive }) => {
+                            return isActive ? "bg-pink-400 rounded-md px-2 py-1" : undefined;
+                        }}>Home</NavLink>
+                        <NavLink to='/topics' className={({ isActive }) => {
+                            return isActive ? "bg-pink-400 rounded-md px-2 py-1" : undefined;
+                        }}>Topics</NavLink>
+                        <NavLink to='/statics' className={({ isActive }) => {
+                            return isActive ? "bg-pink-400 rounded-md px-2 py-1" : undefined;
+                        }}>Statics</NavLink>
+                        <NavLink to='/blog' className={({ isActive }) => {
+                            return isActive ? "bg-pink-400 rounded-md px-2 py-1" : undefined;
+                        }}>Blog</NavLink>
+                    </ul>
+                </div>
             </div>
-            <div className="flex-none">
-                <ul className="menu menu-horizontal p-0 font-bold">
-                    <NavLink to='/home'className={({ isActive }) => {
-                        return isActive ? "bg-pink-400 rounded-md px-2 py-1" : undefined;
-                    }}>Home</NavLink>
-                    <NavLink to='/topics'className={({ isActive }) => {
-                        return isActive ? "bg-pink-400 rounded-md px-2 py-1" : undefined;
-                    }}>Topics</NavLink>
-                    <NavLink to='/statics'className={({ isActive }) => {
-                        return isActive ? "bg-pink-400 rounded-md px-2 py-1" : undefined;
-                    }}>Statics</NavLink>
-                    <NavLink to='/blog'className={({ isActive }) => {
-                        return isActive ? "bg-pink-400 rounded-md px-2 py-1" : undefined;
-                    }}>Blog</NavLink>
-                </ul>
-            </div>
-        </div>
+        </nav>
+        
     );
 };
 
