903b460d4ac9b2367218c01ae084154833a98d5f
shihab00027 <shihabhasan981@gmail.com>
2022-10-11 12:25:13 +0600

    Topic Cart Added

--- src/components/Quiz/Quiz.js
+++ src/components/Quiz/Quiz.js
@@ -4,15 +4,19 @@ import 'react-toastify/dist/ReactToastify.css';
 
 const Quiz = ({ quiz }) => {
     const { id, question, correctAnswer } = quiz;
+    const singleQuiz = quiz.options;
     const correct = () => toast("WOw Correct Answer!")
     const incorrect = () => toast("Oops Wrong Answer!")
     return (
         <div>
             <h1 className='w-[800px] mx-auto font-bold text-xl'>Quiz: {question}</h1>
-            <div onClick={correct} className='border my-6  p-5 font-bold'>01: <button>{quiz.options[0] ? quiz.options[0] : 'No Option'}</button></div>
+            {/* <div onClick={correct} className='border my-6  p-5 font-bold'>01: <button>{quiz.options[0] ? quiz.options[0] : 'No Option'}</button></div>
             <div className='border my-6 p-5 font-bold'>02: <button>{quiz.options[1] ? quiz.options[1] : 'No Option'}</button></div>
             <div className='border my-6 p-5 font-bold'>03: <button>{quiz.options[2] ? quiz.options[2] : 'No Option'}</button></div>
-            <div className='border my-6 p-5 font-bold'>04: <button>{quiz.options[3] ? quiz.options[3] : 'No Option'}</button></div>
+            <div className='border my-6 p-5 font-bold'>04: <button>{quiz.options[3] ? quiz.options[3] : 'No Option'}</button></div> */}
+            {
+                singleQuiz.map(oneQuiz => <li>{oneQuiz}</li>)
+            }
             <ToastContainer />
         </div>
     );
--- src/components/QuizTopic/QuizTopic.js
+++ src/components/QuizTopic/QuizTopic.js
@@ -5,7 +5,7 @@ import Quiz from '../Quiz/Quiz';
 const QuizTopic = () => {
     const quizData = useLoaderData();
     const quizes = quizData.data.questions;
-    console.log(quizData.data.questions);
+    // console.log(quizData.data.questions);
     return (
         <div>
             <h1>this is quiz topic: {quizes.length}</h1>
